ca218119f79149a06418a1991c59f5d364f1f841
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-07-23 18:05:30 -0300

    "Creating class ClienteHttp"

--- src/App.java
+++ src/App.java
@@ -1,10 +1,5 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -16,16 +11,13 @@ public class App {
         String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-07-19&end_date=2022-07-20";
 
 
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
        
 
         //pegar só os dados que nos interessam (título, poster, classificação)
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeConteudos = parser.parse(body);
+        List<Map<String, String>> listaDeConteudos = parser.parse(json);
   
         //exibir e manipular os dados
         var geradora = new GeradorDeFigurinhas();
