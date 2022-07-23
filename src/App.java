46d2e26a51522db45b0907cf42ebed5479928361
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-07-23 12:05:26 -0300

    "Consuming NASA's API"

--- src/App.java
+++ src/App.java
@@ -12,7 +12,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         //fazer uma conexão HTTP e buscar o top 250 filmes
-        String url = "https://api.mocki.io/v2/549a5d8b";
+        //String url = "https://api.mocki.io/v2/549a5d8b";
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-07-19&end_date=2022-07-20";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -27,13 +28,16 @@ public class App {
         //exibir e manipular os dados
         var geradora = new GeradorDeFigurinhas();
         for (Map<String,String> filme : listaDeFilmes){
+            //for(int i = 0; i < 10; i++){
+                //Map<String,String> filme = listaDeFilmes.get(i);
 
 
-            String urlImagem = filme.get("image");
+            String urlImagem = filme.get("url");
+            //.replaceAll("(@+)(.*).jpg$", "$1.jpg");
+                //filme.get("image");
+                
             String titulo = filme.get("title");
             InputStream inputStream = new URL(urlImagem).openStream();
-
-
             String nomeArquivo = "saida/" + titulo + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
