74e049704ffe2b06b0d55ca9365b7fe32958cedf
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-07-22 21:43:36 -0300

    "Setting font style"

--- src/GeradorDeFigurinhas.java
+++ src/GeradorDeFigurinhas.java
@@ -3,6 +3,9 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
+import java.awt.Color;
+import java.awt.Font;
+
 public class GeradorDeFigurinhas {
 
    /**
@@ -23,8 +26,13 @@ public void cria() throws Exception{
     Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
     graphics.drawImage(imagemOriginal, 0, 0, null);
 
+    // configurar a fonte
+    var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+    graphics.setColor(Color.YELLOW);
+    graphics.setFont(fonte);
+
     // escrever uma frase na nova imagem 
-    graphics.drawString("TOPZERA", 0, novaAltura - 100);
+    graphics.drawString("TOPZERA", 175, novaAltura - 100);
 
     // escrever a nova imagem em um arquivo
     ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
